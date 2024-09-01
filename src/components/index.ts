a705667f788630fedaade34392286434de9f86a3
Benz Peerakit <peerakitzaa001@hotmail.com>
2024-09-01 16:48:59 +0700

    'PrjectComponent'

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+import ProjectsPage from './projects/ProjectsPage';
+
+export { ProjectsPage };
---
+++ src/components/projects/ProjectList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Project } from './Project';
+import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
+
+interface ProjectListProps {
+  projects: Project[];
+}
+
+function ProjectList({ projects }: ProjectListProps) {
+  const item = projects.map((value) => (
+    <div key={value.id} className="col-sm">
+      <ProjectCard project={value} />
+      <ProjectForm />
+    </div>
+  ));
+
+  return <div className="row">{item}</div>;
+}
+
+export default ProjectList;
